dd03f2b393caa9e14b56449cc981e71bc51c9ff6
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-18 16:24:39 -0400

    Export `flip_x_discrete()`

--- R/ggplot2-internal.R
+++ R/ggplot2-internal.R
@@ -1,3 +1,19 @@
+.detectMapping <- function(object) {
+    assert(is(object, "ggplot"))
+
+    # First, check for `aes()` in `ggplot()` call.
+    map <- object[["mapping"]]
+    if (hasLength(map)) return(map)
+
+    # Second, check the first layer defined in the object.
+    map <- object[["layers"]][[1L]][["mapping"]]
+    if (hasLength(map)) return(map)
+
+    stop("Failed to detect mapping.")
+}
+
+
+
 .geneMedianLine <- stat_summary(
     fun.y = median,
     fun.ymin = median,
