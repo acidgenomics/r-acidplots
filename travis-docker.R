280f05f9b0910e45322d17070a5c79f1dae13908
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-05 12:57:53 -0400

    Pin to R 3.5

--- travis-docker.R
+++ travis-docker.R
@@ -1,5 +1,20 @@
+#!/usr/bin/env Rscript
+
 Sys.setenv(TZ = "America/New_York")
+options(
+    deparse.max.lines = 3L,
+    error = quote(rlang::entrace()),
+    rlang_backtrace_on_error = "full",
+    showErrorCalls = TRUE,
+    showWarnCalls = TRUE,
+    warn = 1L,
+    warnPartialMatchAttr = TRUE,
+    warnPartialMatchDollar = TRUE,
+    warning.length = 8170L
+)
+
 sessioninfo::session_info()
+
 rcmdcheck::rcmdcheck(
     path = ".",
     args = c(
@@ -18,7 +33,7 @@ rcmdcheck::rcmdcheck(
 if (packageVersion("base") >= "3.6") {
     BiocCheck::BiocCheck(
         package = ".",
-        `quit-with-status` = FALSE
+        `quit-with-status` = TRUE
     )
     lintr::lint_package()
 }
