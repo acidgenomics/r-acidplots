7b5bf47ae919de178875c45d0b724a8ad4aaa761
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-07 11:53:39 -0400

    Simplify passthrough to UpSetR::upset so it doesn't break in future releases

--- R/upset.R
+++ R/upset.R
@@ -1,7 +1,12 @@
-#' UpSetR plot, with improved default aesthetics
+#' UpSetR plot
+#'
+#' Wrapper for [UpSetR::upset()] with improved default aesthetics.
+#'
 #' @export
-#' @inherit UpSetR::upset description details params references
+#' @inherit UpSetR::upset details params references
+#' @inheritParams params
 #' @return Graphical output, no return.
+#'
 #' @examples
 #' ## Refer to UpSetR::upset() documentation for more examples.
 #' movies <- read.csv(
@@ -11,20 +16,35 @@
 #' )
 #' upset(movies)
 # Last modified 2019-06-07.
-upset <- UpSetR::upset
-
-f <- formals(upset)
-f[["line.size"]] <- 1L
-f[["main.bar.color"]] <- "black"
-f[["matrix.color"]] <- "black"
-f[["matrix.dot.alpha"]] <- 1L
-f[["mb.ratio"]] <- quote(c(0.6, 0.4))
-f[["point.size"]] <- 3L
-f[["sets.bar.color"]] <- "black"
-f[["shade.alpha"]] <- 1L
-f[["shade.color"]] <- NA
-f[["text.scale"]] <- 1.5
-# Fix T/F usage in UpSetR package.
-f[["decreasing"]] <- c(TRUE, FALSE)
-f[["keep.order"]] <- FALSE
-formals(upset) <- f
+upset <- function(
+    ...,
+    line.size = 1L,
+    main.bar.color = "black",
+    matrix.color = "black",
+    matrix.dot.alpha = 1L,
+    mb.ratio = c(0.6, 0.4),
+    point.size = 3L,
+    sets.bar.color = "black",
+    shade.alpha = 1L,
+    shade.color = NA,
+    text.scale = 1.5,
+    # Fix T/F usage in UpSetR package.
+    decreasing = c(TRUE, FALSE),
+    keep.order = FALSE
+) {
+    UpSetR::upset(
+        ...,
+        line.size = line.size,
+        main.bar.color = main.bar.color,
+        matrix.color = matrix.color,
+        matrix.dot.alpha = matrix.dot.alpha,
+        mb.ratio = mb.ratio,
+        point.size = point.size,
+        sets.bar.color = sets.bar.color,
+        shade.alpha = shade.alpha,
+        shade.color = shade.color,
+        text.scale = text.scale,
+        decreasing = decreasing,
+        keep.order = keep.order
+    )
+}
