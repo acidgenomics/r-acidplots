d1de0a9bcf1a2c8a180b030383733c433c7cba4e
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-23 11:28:15 -0400

    Add code coverage

--- tests/testthat/test-plotCountsPerBiotype.R
+++ tests/testthat/test-plotCountsPerBiotype.R
@@ -1,5 +1,15 @@
 context("plotCountsPerBiotype")
 
+test_that("SummarizedExperiment", {
+    x <- plotCountsPerBiotype(rse)
+    expect_s3_class(x, "ggplot")
+})
+
 
 
 context("plotCountsPerBroadClass")
+
+test_that("SummarizedExperiment", {
+    x <- plotCountsPerBroadClass(rse)
+    expect_s3_class(x, "ggplot")
+})
