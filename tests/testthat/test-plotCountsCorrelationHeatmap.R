334132b59eb773dab2d2d5140e04ebd3218991d4
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-23 11:45:27 -0400

    Improve code coverage

---
+++ tests/testthat/test-plotCountsCorrelationHeatmap.R
@@ -0,0 +1,8 @@
+context("plotCountsCorrelationHeatmap")
+
+test_that("matrix", {
+    x <- assay(rse)
+    y <- x + 1L
+    p <- plotCountsCorrelationHeatmap(x, y)
+    expect_s3_class(p, "pheatmap")
+})
