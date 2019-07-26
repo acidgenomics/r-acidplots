fa1ff2ac9cc11c8ac8b9f6d02dc38bb69b7b080b
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-26 06:42:18 -0400

    Update coverage.R

--- coverage.R
+++ coverage.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 ## Check package coverage with covr.
-## Updated 2019-07-22.
+## Updated 2019-07-26.
 
 options(
     error = quote(quit(status = 1L)),
@@ -18,7 +18,7 @@ requireNamespace("covr", quietly = TRUE)
 cov <- covr::package_coverage()
 pct <- covr::percent_coverage(cov)
 
-if (pct < 90L) {
+if (pct < 80L) {
     print(cov)
     stop(sprintf("Coverage is %s%%.", round(pct, digits = 2L)))
 }
