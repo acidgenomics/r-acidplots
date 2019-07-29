fb999e5ca5f14f9dd654dcce4c67a178112e680c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-29 15:23:14 -0400

    Add gender marker code coverage

--- tests/testthat/test-plotGenderMarkers.R
+++ tests/testthat/test-plotGenderMarkers.R
@@ -1,1 +1,21 @@
 context("plotGenderMarkers")
+
+test_that("SummarizedExperiment", {
+    object <- rse
+    data(genderMarkers, envir = environment())
+    organism <- "Homo sapiens"
+    expect_identical(organism(object), organism)
+    markers <- genderMarkers[[camel(organism(object))]]
+    geneIDs <- markers[["geneID"]]
+    geneNames <- markers[["geneName"]]
+    seq <- seq_len(nrow(markers))
+    rownames(object)[seq] <- geneIDs
+    rowData(object)[["geneID"]] <- as.character(rowData(object)[["geneID"]])
+    rowData(object)[["geneID"]][seq] <- geneIDs
+    rowData(object)[["geneName"]] <-
+        as.character(rowData(object)[["geneName"]])
+    rowData(object)[["geneName"]][seq] <- geneNames
+    p <- plotGenderMarkers(object)
+    expect_s3_class(p, "ggplot")
+    expect_true(all(geneNames %in% p$data[["rowname"]]))
+})
