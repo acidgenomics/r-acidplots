a28001c199f66d9f7e105e25feb218dc048c6c38
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-12 12:59:20 -0400

    Add plotQC method support for SingleCellExperiment

--- R/plotQC-methods.R
+++ R/plotQC-methods.R
@@ -43,30 +43,22 @@ NULL
 
 
 
-## Updated 2019-07-23.
+## Updated 2019-08-12.
 `plotQC,SummarizedExperiment` <-  # nolint
     function(object, assay = 1L) {
         validObject(object)
         assert(isScalar(assay))
 
-        ## Always coerce to dense matrix.
-        mat <- as.matrix(assays(object)[[assay]])
-
-        ## Total counts.
         totalCounts <- plotTotalCounts(object, assay = assay)
-
-        ## Dropout rate.
         zerosVsDepth <- plotZerosVsDepth(object, assay = assay)
 
-        ## Counts per row (gene).
+        ## Counts per row or column.
+        mat <- as.matrix(assay(object, i = assay))
         rowSums <- .plotSumsECDF(mat, fun = rowSums) +
-            labs(title = "counts per row")
-
-        ## Counts per column (sample).
+            labs(title = "Counts per row")
         colSums <- .plotSumsECDF(mat, fun = colSums) +
-            labs(title = "counts per column")
+            labs(title = "Counts per column")
 
-        ## Return paneled plot.
         plot_grid(
             plotlist = list(
                 totalCounts = totalCounts,
@@ -85,4 +77,108 @@ setMethod(
     f = "plotQC",
     signature = signature("SummarizedExperiment"),
     definition = `plotQC,SummarizedExperiment`
+)
+
+
+
+## Updated 2019-08-11.
+`plotQC,SingleCellExperiment` <-  # nolint
+    function(object) {
+        ## FIXME Need to add `hasMetrics()` from goalie.
+        assert(hasMetrics(object))
+    }
+
+
+
+#' @rdname plotQC
+#' @export
+setMethod(
+    f = "plotQC",
+    signature = signature("SingleCellExperiment"),
+    definition = `plotQC,SingleCellExperiment`
+)
+
+
+
+
+## Updated 2019-08-12.
+`plotQC,SingleCellExperiment` <-  # nolint
+    function(
+        object,
+        assay = 1L,
+        interestingGroups = NULL,
+        geom,
+        legend
+    ) {
+        validObject(object)
+        assert(
+            isScalar(assay),
+            isFlag(legend)
+        )
+        if (!hasMetrics(object, colData = c("nCount", "nFeature"))) {
+            object <- calculateMetrics(object)
+        }
+        geom <- match.arg(geom)
+        interestingGroups(object) <-
+            matchInterestingGroups(object, interestingGroups)
+
+        ## Don't show cell counts for unfiltered datasets.
+        if (hasSubset(object, metadata = "filterCells")) {
+            cellCounts <- plotCellCounts(object)
+            zerosVsDepth <- NULL
+        } else {
+            cellCounts <- NULL
+            zerosVsDepth <- plotZerosVsDepth(object)
+        }
+
+        countsPerCell <- plotCountsPerCell(object, geom = geom)
+        featuresPerCell <- plotFeaturesPerCell(object, geom = geom)
+        countsVsFeatures <- plotCountsVsFeatures(object)
+        novelty <- plotNovelty(object, geom = geom)
+        ## This isn't defined for all objects.
+        mitoRatio <- tryCatch(
+            expr = plotMitoRatio(object, geom = geom),
+            error = function(e) {
+                NULL
+            }
+        )
+
+        plotlist <- list(
+            cellCounts = cellCounts,
+            countsPerCell = countsPerCell,
+            featuresPerCell = featuresPerCell,
+            countsVsFeatures = countsVsFeatures,
+            novelty = novelty,
+            mitoRatio = mitoRatio,
+            zerosVsDepth = zerosVsDepth
+        )
+
+        ## Remove any `NULL` plots. This is useful for nuking the
+        ## `plotReadsPerCell` return on an object that doesn't contain raw
+        ## cellular barcode counts.
+        plotlist <- Filter(f = Negate(is.null), x = plotlist)
+
+        ## Hide the legends, if desired.
+        if (identical(legend, FALSE)) {
+            .hideLegend <- function(gg) {
+                gg + theme(legend.position = "none")
+            }
+            plotlist <- lapply(plotlist, .hideLegend)
+        }
+
+        ## Return.
+        plot_grid(plotlist = plotlist)
+    }
+
+formals(`plotQC,SingleCellExperiment`)[["geom"]] <- geom
+formals(`plotQC,SingleCellExperiment`)[["legend"]] <- formalsList[["legend"]]
+
+
+
+#' @rdname plotQC
+#' @export
+setMethod(
+    f = "plotQC",
+    signature = signature("SingleCellExperiment"),
+    definition = `plotQC,SingleCellExperiment`
 )
