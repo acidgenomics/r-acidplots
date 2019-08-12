0f254fbba5460c8802efaee792e18beb5631f50a
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-12 13:55:58 -0400

    Split out `hideLegendsInPlotlist()` function

--- R/ggplot2-internal.R
+++ R/ggplot2-internal.R
@@ -1,13 +1,22 @@
+## Updated 2019-08-12.
 .detectMapping <- function(object) {
     assert(is(object, "ggplot"))
-
     ## First, check for `aes()` in `ggplot()` call.
     map <- object[["mapping"]]
     if (hasLength(map)) return(map)
-
     ## Second, check the first layer defined in the object.
     map <- object[["layers"]][[1L]][["mapping"]]
     if (hasLength(map)) return(map)
-
     stop("Failed to detect mapping.")
+}
+
+
+
+## Updated 2019-08-12.
+.hideLegendsInPlotlist <- function(plotlist) {
+    assert(is.list(plotlist))
+    hideLegend <- function(gg) {
+        gg + theme(legend.position = "none")
+    }
+    lapply(X = plotlist, FUN = hideLegend)
 }
