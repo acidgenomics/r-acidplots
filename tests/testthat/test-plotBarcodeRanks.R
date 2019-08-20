0b7e70989947be20c3a2bc00209ed5298ca7bd14
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-20 09:50:35 -0400

    Update unit test skips

--- tests/testthat/test-plotBarcodeRanks.R
+++ tests/testthat/test-plotBarcodeRanks.R
@@ -1,6 +1,6 @@
 context("plotBarcodeRanks")
 
-skip_if_not(packageVersion("DropletUtils") >= "1.4")
+skip_if_not_installed(pkg = "DropletUtils", minimum_version = "1.4")
 
 test_that("SingleCellExperiment", {
     p <- plotBarcodeRanks(sce)
