63887a42ea39ce4344aac1ed1bd1a4265ba60fc5
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-21 05:31:33 -0400

    Use left_join that supports DataFrame

--- R/imports.R
+++ R/imports.R
@@ -17,7 +17,7 @@
 #' @importFrom basejump Gene2Symbol aggregateCellsToSamples
 #'   as.SummarizedExperiment as_tibble camelCase cell2sample
 #'   convertGenesToSymbols counts decode formalsList interestingGroups
-#'   interestingGroups<- makeLabel makeTitle matchArgsToDoCall
+#'   interestingGroups<- left_join makeLabel makeTitle matchArgsToDoCall
 #'   matchInterestingGroups mapGenesToRownames meltCounts methodFormals metrics
 #'   organism sampleData snakeCase standardizeCall zerosVsDepth
 #' @importFrom cowplot plot_grid
@@ -48,7 +48,7 @@
 #'
 #'
 #'
-#' @importFrom dplyr arrange bind_rows case_when desc filter group_by left_join
+#' @importFrom dplyr arrange bind_rows case_when desc filter group_by
 #'   mutate mutate_all mutate_at mutate_if n pull rename select select_if slice
 #'   summarise summarise_all top_n ungroup
 #' @importFrom magrittr %>%
