7e169c0189e1cdfde00795187326d74b68cd7593
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-15 21:16:08 -0400

    Simplify internal code

--- R/plotMitoRatio-methods.R
+++ R/plotMitoRatio-methods.R
@@ -41,13 +41,12 @@ NULL
         labels
     ) {
         assert(isInLeftOpenRange(max, lower = 0L, upper = 1L))
-        geom <- match.arg(geom)
         do.call(
             what = .plotQCMetric,
             args = list(
                 object = object,
                 metricCol = "mitoRatio",
-                geom = geom,
+                geom = match.arg(geom),
                 interestingGroups = interestingGroups,
                 max = max,
                 trans = trans,
