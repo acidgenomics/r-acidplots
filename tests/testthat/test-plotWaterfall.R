2c6c0800ad497c77d51890e6b57cbf5c7b52daee
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-25 14:47:27 -0400

    Add coverage

---
+++ tests/testthat/test-plotWaterfall.R
@@ -0,0 +1,30 @@
+context("plotWaterfall")
+
+test_that("data.frame", {
+    object <- data.frame(
+        cell_id = paste("cell", seq_len(12L), sep = "_"),
+        ic50 = seq(
+        from = 0.1,
+            to = 10L,
+            length.out = 12L
+        ),
+        tumor_type = rep(
+            x = c("breast", "bladder"),
+            times = 6L
+        ),
+        tumor_subtype = rep(
+            x = c("benign", "malignant"),
+            each = 6L
+        )
+    )
+    p <- plotWaterfall(
+        object = object,
+        xCol = "cell_id",
+        yCol = "ic50",
+        interestingGroups = c("tumor_type", "tumor_subtype"),
+        labels = list(
+            title = "Effect of compound on cell survival"
+        )
+    )
+    expect_s3_class(p, "ggplot")
+})
