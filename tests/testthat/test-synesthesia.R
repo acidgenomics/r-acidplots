df38fc9d4ec2ad62d811318c3c1a6d264c3a7531
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-25 09:50:33 -0400

    Remove patrick dependency for unit tests

--- tests/testthat/test-synesthesia.R
+++ tests/testthat/test-synesthesia.R
@@ -6,15 +6,14 @@ test_that("synesthesia_pal", {
     expect_true(allAreHexColors(synesthesia_pal()(n = 2L)))
 })
 
-with_parameters_test_that(
-    "ggplot2 scales", {
-        expect_is(f, "function")
-        expect_s3_class(f(), "Scale")
-    },
-    f = list(
+test_that("ggplot2 scales", {
+    for (f in list(
         scale_color_synesthesia_c,
         scale_color_synesthesia_d,
         scale_fill_synesthesia_c,
         scale_fill_synesthesia_d
-    )
-)
+    )) {
+        expect_is(f, "function")
+        expect_s3_class(f(), "Scale")
+    }
+})
