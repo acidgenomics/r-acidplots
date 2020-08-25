f7353358fa1fabbc007189f0b78c6ee0e388dc69
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-25 13:19:15 -0400

    Improve unit test

--- tests/testthat/test-plotPCA.R
+++ tests/testthat/test-plotPCA.R
@@ -3,7 +3,8 @@ context("plotPCA")
 test_that("SummarizedExperiment", {
     p <- plotPCA(rse, label = FALSE)
     expect_s3_class(p, "ggplot")
-
     p <- plotPCA(rse, label = TRUE)
     expect_s3_class(p, "ggplot")
+    ## Check that data frame saved inside ggplot object has attributes.
+    expect_is(attr(p[["data"]], "percentVar"), "numeric")
 })
