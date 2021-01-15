8fb8c4db5893fa0c193c96c81111232308d340b4
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-15 09:30:39 -0500

    Improve error message

--- R/internal-plotQC.R
+++ R/internal-plotQC.R
@@ -1,6 +1,7 @@
 #' Match sample identifier column
 #'
 #' @note Updated 2021-01-15.
+#' @note Consider adding this into basejump in a future update.
 #' @noRd
 #'
 #' @details
@@ -10,7 +11,7 @@
 #' - `sampleid` (unused).
 #' - `sample` (last resort).
 #'
-#' @return `character(1)` or `NULL` (on match failure).
+#' @return `character(1)` or error on match failure.
 #'
 #' @examples
 #' data(SingleCellExperiment, package = "AcidTest")
@@ -21,7 +22,15 @@
     x <- colnames(colData(object))
     table <- c("sampleId", "sampleID", "sampleid", "sample")
     match <- match(x = x, table = table)
-    if (all(is.na(match))) return(NULL)
+    if (all(is.na(match))) {
+        stop(sprintf(
+            paste0(
+                "Failed to match sample identifier in '%s()'.\n",
+                "Expecting (in order of preference): %s."
+            ),
+            "colData", toString(table)
+        ))
+    }
     id <- table[min(na.omit(match))]
     id
 }
