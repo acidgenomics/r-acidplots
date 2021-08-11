cb5ce4a8aa44db878a4c18c91e3184a9ab142f5d
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-11 14:11:44 -0400

    Fix inflection and knee point labeling

--- R/plotBarcodeRanks-methods.R
+++ R/plotBarcodeRanks-methods.R
@@ -1,7 +1,7 @@
 #' @name plotBarcodeRanks
 #' @inherit AcidGenerics::plotBarcodeRanks
 #' @note Requires DropletUtils package to be installed.
-#' @note Updated 2019-09-15.
+#' @note Updated 2021-08-11.
 #'
 #' @param colors `character(3)`.
 #'   Character vector denoting `fitline`, `inflection`, and `knee` point colors.
@@ -20,31 +20,37 @@ NULL
 
 
 
-## Updated 2019-09-15.
+## Updated 2021-08-11.
 `plotBarcodeRanks,SingleCellExperiment` <-  # nolint
     function(
         object,
         colors = c(
-            fitline = AcidPlots::lightPalette[["blue"]],
-            inflection = AcidPlots::lightPalette[["purple"]],
-            knee = AcidPlots::lightPalette[["orange"]]
+            "fitline" = AcidPlots::lightPalette[["blue"]],
+            "inflection" = AcidPlots::lightPalette[["purple"]],
+            "knee" = AcidPlots::lightPalette[["orange"]]
+        ),
+        labels = list(
+            "title" = NULL,
+            "subtitle" = NULL
         ),
         ...
     ) {
+        requireNamespaces("DropletUtils")
         validObject(object)
-        dots <- list(...)
         assert(
-            requireNamespace("DropletUtils", quietly = TRUE),
             isCharacter(colors),
             areSetEqual(
                 x = names(colors),
                 y = c("fitline", "inflection", "knee")
             )
         )
-        ranksPerSample <- do.call(
-            what = barcodeRanksPerSample,
-            args = c(object = object, dots)
+        labels <- matchLabels(
+            labels = labels,
+            choices = eval(formals()[["labels"]])
         )
+        ## FIXME Need to update the internal code here?
+        ## FIXME Doesn't seem to be calculating correctly now.
+        ranksPerSample <- barcodeRanksPerSample(object, ...)
         sampleData <- sampleData(object)
         if (is.null(sampleData)) {
             sampleNames <- "unknown"
@@ -63,6 +69,18 @@ NULL
                 data <- as_tibble(ranks, rownames = NULL)
                 inflection <- metadata(ranks)[["inflection"]]
                 knee <- metadata(ranks)[["knee"]]
+                ## Label the knee and inflection points more clearly
+                whichKnee <- which.min(abs(data[["total"]] - knee))
+                whichInflection <- which.min(abs(data[["total"]] - inflection))
+                labelData <-data[c(whichKnee, whichInflection), , drop = FALSE]
+                labelData[["label"]] <- c(
+                    paste("knee", "=", knee),
+                    paste("inflection", "=", inflection)
+                )
+                ## Include the fit line (smooth.spline), if possible.
+                fitData <- data
+                keep <- which(!is.na(fitData[["fitted"]]))
+                fitData <- fitData[keep, , drop = FALSE]
                 p <- ggplot(data = data) +
                     geom_point(
                         mapping = aes(
@@ -76,19 +94,17 @@ NULL
                         title = sampleName,
                         y = "counts per cell"
                     )
-                ## Include the fit line (smooth.spline).
-                fitData <- data
-                keep <- which(!is.na(fitData[["fitted"]]))
-                fitData <- fitData[keep, , drop = FALSE]
-                p <- p + geom_line(
-                    data = fitData,
-                    mapping = aes(
-                        x = !!sym("rank"),
-                        y = !!sym("fitted")
-                    ),
-                    color = colors[["fitline"]],
-                    size = 1L
-                )
+                if (hasRows(fitData)) {
+                    p <- p + geom_line(
+                        data = fitData,
+                        mapping = aes(
+                            x = !!sym("rank"),
+                            y = !!sym("fitted")
+                        ),
+                        color = colors[["fitline"]],
+                        size = 1L
+                    )
+                }
                 p <- p +
                     geom_hline(
                         color = colors[["knee"]],
@@ -100,14 +116,6 @@ NULL
                         linetype = "dashed",
                         yintercept = inflection
                     )
-                ## Label the knee and inflection points more clearly
-                knee <- which.min(abs(data[["total"]] - knee))
-                inflection <- which.min(abs(data[["total"]] - inflection))
-                labelData <- data[c(knee, inflection), , drop = FALSE]
-                labelData[["label"]] <- c(
-                    paste("knee", "=", knee),
-                    paste("inflection", "=", inflection)
-                )
                 p +
                     acid_geom_label_repel(
                         data = labelData,
@@ -124,8 +132,14 @@ NULL
         )
         ## Sort the plots by sample name.
         plotlist <- plotlist[sort(names(plotlist))]
-        ## FIXME Migrate away from this.
-        plot_grid(plotlist = plotlist)
+        ## Using patchwork package to dynamically arrange the plots.
+        p <- wrap_plots(plotlist)
+        ## Support title and/or subtitle labeling.
+        p <- p + plot_annotation(
+            "title" = labels[["title"]],
+            "subtitle" = labels[["subtitle"]]
+        )
+        p
     }
 
 
