04613e8a4f26e295928fe793c4b2099a76a17402
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-11 12:56:19 -0400

    Improve internal code for palette handling

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -67,6 +67,16 @@ NULL
 
 
 
+#' Prepare exported palette global variable
+#'
+#' @note Updated 2021-08-11.
+#' @noRd
+.preparePalette <- function(name) {
+    .palettes[[name]][sort(names(.palettes[[name]]))]
+}
+
+
+
 #' Improve the `rgb()` default to use 0:255, as expected
 #'
 #' @note Updated 2019-09-13.
@@ -77,90 +87,92 @@ NULL
 
 
 
-## FIXME Put these into a list, and reexport below...
-
-iOSLightPalette <- c(
-    "background" = .rgb(255L, 255L, 255L),
-    "foreground" = .rgb(0L, 0L, 0L),
-    "border" = .rgb(142L, 142L, 147L),
-    "blue" = .rgb(0L, 122L, 255L),
-    "gray" = .rgb(142L, 142L, 147L),
-    "green" = .rgb(52L, 199L, 89L),
-    "orange" = .rgb(255L, 149L, 0L),
-    "pink" = .rgb(255L, 45L, 85L),
-    "purple" = .rgb(175L, 82L, 222L),
-    "red" = .rgb(255L, 59L, 48L),
-    "teal" = .rgb(90L, 200L, 250L),
-    "yellow" = .rgb(255L, 204L, 0L)
+## Updated 2021-08-11.
+.palettes <- list(
+    "dracula" = c(
+        "background" = .rgb(40L, 42L, 54L),
+        "foreground" = .rgb(248L, 248L, 242L),
+        "border" = .rgb(54L, 57L, 72L),
+        "blue" = .rgb(0L, 122L, 255L),
+        "green" = .rgb(52L, 199L, 89L),
+        "orange" = .rgb(255L, 149L, 0L),
+        "pink" = .rgb(255L, 45L, 85L),
+        "purple" = .rgb(175L, 82L, 222L),
+        "red" = .rgb(255L, 59L, 48L),
+        "teal" = .rgb(90L, 200L, 250L),
+        "yellow" = .rgb(255L, 204L, 0L)
+    ),
+    "iOSDark" = c(
+        "background" = .rgb(23L, 23L, 23L),
+        "foreground" = .rgb(153L, 153L, 153L),
+        "border" = .rgb(152L, 152L, 157L),
+        "blue" = .rgb(10L, 132L, 255L),
+        "gray" = .rgb(152L, 152L, 157L),
+        "green" = .rgb(48L, 209L, 88L),
+        "orange" = .rgb(255L, 159L, 10L),
+        "pink" = .rgb(255L, 55L, 95L),
+        "purple" = .rgb(191L, 90L, 242L),
+        "red" = .rgb(255L, 69L, 58L),
+        "teal" = .rgb(100L, 210L, 255L),
+        "yellow" = .rgb(255L, 214L, 10L)
+    ),
+    "iOSLight" = c(
+        "background" = .rgb(255L, 255L, 255L),
+        "foreground" = .rgb(0L, 0L, 0L),
+        "border" = .rgb(142L, 142L, 147L),
+        "blue" = .rgb(0L, 122L, 255L),
+        "gray" = .rgb(142L, 142L, 147L),
+        "green" = .rgb(52L, 199L, 89L),
+        "orange" = .rgb(255L, 149L, 0L),
+        "pink" = .rgb(255L, 45L, 85L),
+        "purple" = .rgb(175L, 82L, 222L),
+        "red" = .rgb(255L, 59L, 48L),
+        "teal" = .rgb(90L, 200L, 250L),
+        "yellow" = .rgb(255L, 204L, 0L)
+    )
 )
 
-iOSDarkPalette <- c(
-    "background" = .rgb(23L, 23L, 23L),
-    "foreground" = .rgb(153L, 153L, 153L),
-    "border" = .rgb(152L, 152L, 157L),
-    "blue" = .rgb(10L, 132L, 255L),
-    "gray" = .rgb(152L, 152L, 157L),
-    "green" = .rgb(48L, 209L, 88L),
-    "orange" = .rgb(255L, 159L, 10L),
-    "pink" = .rgb(255L, 55L, 95L),
-    "purple" = .rgb(191L, 90L, 242L),
-    "red" = .rgb(255L, 69L, 58L),
-    "teal" = .rgb(100L, 210L, 255L),
-    "yellow" = .rgb(255L, 214L, 10L)
-)
+.palettes[["macOSDark"]] <- .palettes[["iOSDark"]]
+.palettes[["macOSDark"]][["green"]] <- .rgb(50L, 215L, 75L)
 
-macOSLightPalette <- iOSLightPalette
-macOSLightPalette[["green"]] <- .rgb(40L, 205L, 65L)
+.palettes[["macOSLight"]] <- .palettes[["iOSLight"]]
+.palettes[["macOSLight"]][["green"]] <- .rgb(40L, 205L, 65L)
 
-macOSDarkPalette <- iOSDarkPalette
-macOSDarkPalette[["green"]] <- .rgb(50L, 215L, 75L)
+.palettes[["dracula"]][["gray"]] <- .palettes[["macOSDark"]][["gray"]]
 
-draculaPalette <- c(
-    "background" = .rgb(40L, 42L, 54L),
-    "foreground" = .rgb(248L, 248L, 242L),
-    "border" = .rgb(54L, 57L, 72L),
-    "blue" = .rgb(0L, 122L, 255L),
-    "green" = .rgb(52L, 199L, 89L),
-    "orange" = .rgb(255L, 149L, 0L),
-    "pink" = .rgb(255L, 45L, 85L),
-    "purple" = .rgb(175L, 82L, 222L),
-    "red" = .rgb(255L, 59L, 48L),
-    "teal" = .rgb(90L, 200L, 250L),
-    "yellow" = .rgb(255L, 204L, 0L)
-)
-draculaPalette[["gray"]] <- macOSDarkPalette[["gray"]]
+.palettes[["light"]] <- .palettes[["macOSLight"]]
 
-lightPalette <- macOSLightPalette
+.palettes[["dark"]] <- .palettes[["macOSDark"]]
+.palettes[["dark"]][["background"]] <- .rgb(0L, 0L, 0L)
+.palettes[["dark"]][["foreground"]] <- .rgb(255L, 255L, 255L)
+.palettes[["dark"]][["gray"]] <- .rgb(26L, 26L, 26L)  ## gray10
 
-darkPalette <- macOSDarkPalette
-darkPalette[["background"]] <- .rgb(0L, 0L, 0L)
-darkPalette[["foreground"]] <- .rgb(255L, 255L, 255L)
-darkPalette[["gray"]] <- .rgb(26L, 26L, 26L)  ## gray10
+
 
 #' @rdname palettes
 #' @export
-lightPalette <- lightPalette[sort(names(lightPalette))]
+lightPalette <- .preparePalette("light")
 
 #' @rdname palettes
 #' @export
-darkPalette <- darkPalette[sort(names(darkPalette))]
+darkPalette <- .preparePalette("dark")
 
 #' @rdname palettes
 #' @export
-draculaPalette <- draculaPalette[sort(names(draculaPalette))]
+draculaPalette <- .preparePalette("dracula")
 
 #' @rdname palettes
 #' @export
-iOSLightPalette <- iOSLightPalette[sort(names(iOSLightPalette))]
+iOSLightPalette <- .preparePalette("iOSLight")
 
 #' @rdname palettes
 #' @export
-iOSDarkPalette <- iOSDarkPalette[sort(names(iOSDarkPalette))]
+iOSDarkPalette <- .preparePalette("iOSDark")
 
 #' @rdname palettes
 #' @export
-macOSLightPalette <- macOSLightPalette[sort(names(macOSLightPalette))]
+macOSLightPalette <- .preparePalette("macOSLight")
 
 #' @rdname palettes
 #' @export
-macOSDarkPalette <- macOSDarkPalette[sort(names(macOSDarkPalette))]
+macOSDarkPalette <- .preparePalette("macOSDark")
