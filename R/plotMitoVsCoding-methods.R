e1af390d8a4aef800fe6fbde4dbbfa371099ec84
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-10 10:15:28 -0400

    Simplify color palette handling

--- R/plotMitoVsCoding-methods.R
+++ R/plotMitoVsCoding-methods.R
@@ -1,7 +1,7 @@
 #' @name plotMitoVsCoding
 #' @author Michael Steinbaugh, Rory Kirchner
 #' @inherit AcidGenerics::plotMitoVsCoding
-#' @note Updated 2019-09-16.
+#' @note Updated 2021-09-10.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @param ... Additional arguments.
@@ -28,40 +28,34 @@ NULL
 
 
 
-## Updated 2019-09-16.
+## Updated 2021-09-10.
 `plotMitoVsCoding,SCE` <-  # nolint
     function(
         object,
         interestingGroups = NULL,
         trendline = FALSE,
-        color,
         trans = "log2",
         labels = list(
-            title = "Mito vs. coding",
-            subtitle = NULL,
-            x = "coding",
-            y = "mito"
+            "title" = "Mito vs. coding",
+            "subtitle" = NULL,
+            "x" = "coding",
+            "y" = "mito"
         )
     ) {
         do.call(
             what = .plotQCScatterplot,
             args = list(
-                object = object,
-                interestingGroups = interestingGroups,
-                trendline = trendline,
-                xCol = "nCoding",
-                yCol = "nMito",
-                color = color,
-                xTrans = trans,
-                yTrans = trans,
-                labels = matchLabels(labels)
+                "object" = object,
+                "interestingGroups" = interestingGroups,
+                "trendline" = trendline,
+                "xCol" = "nCoding",
+                "yCol" = "nMito",
+                "xTrans" = trans,
+                "yTrans" = trans,
+                "labels" = matchLabels(labels)
             )
         )
     }
-
-f <- formals(`plotMitoVsCoding,SCE`)
-f[["color"]] <- formalsList[["color.discrete"]]
-formals(`plotMitoVsCoding,SCE`) <- f
 
 
 
