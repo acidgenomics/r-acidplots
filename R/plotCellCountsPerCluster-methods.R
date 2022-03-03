5907a27ea3c3044690c00ccb80b45f7f4465f7c8
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-03 16:54:29 -0500

    Migrate this code from pointillism

--- R/plotCellCountsPerCluster-methods.R
+++ R/plotCellCountsPerCluster-methods.R
@@ -1,1 +1,78 @@
-## FIXME Need to migrate this from pointillism.
+#' @name plotCellCountsPerCluster
+#' @inherit AcidGenerics::plotCellCountsPerCluster
+#' @note Updated 2022-03-03.
+#'
+#' @inheritParams AcidRoxygen::params
+#' @param ... Additional arguments.
+#'
+#' @return Show graphical output. Invisibly return `ggplot`.
+#'
+#' @examples
+#' data(Seurat, package = "AcidTest")
+#'
+#' ## Seurat ====
+#' object <- Seurat
+#' plotCellCountsPerCluster(object)
+NULL
+
+
+
+## Updated 2019-08-03.
+`plotCellCountsPerCluster,SCE` <-  # nolint
+    function(
+        object,
+        interestingGroups = NULL
+    ) {
+        validObject(object)
+        interestingGroups(object) <-
+            matchInterestingGroups(object, interestingGroups)
+        interestingGroups <- interestingGroups(object)
+        data <- cellCountsPerCluster(object = object)
+        if (isTRUE(length(levels(data[["sampleName"]])) > 1L)) {
+            multipleSamples <- TRUE
+            col <- "interestingGroups"
+            legendTitle <- paste(interestingGroups, collapse = ":\n")
+            showLegend <- TRUE
+            xLab <- NULL
+        } else {
+            multipleSamples <- FALSE
+            col <- "ident"
+            legendTitle <- NA
+            showLegend <- FALSE
+            xLab <- "cluster"
+        }
+        ## Plot.
+        p <- ggplot(
+            data = as.data.frame(data),
+            mapping = aes(
+                x = !!sym(col),
+                y = !!sym("n"),
+                fill = !!sym(col)
+            )
+        ) +
+            geom_bar(
+                stat = "identity",
+                show.legend = showLegend
+            ) +
+            labs(
+                x = xLab,
+                y = "n cells",
+                fill = legendTitle
+            )
+        ## Wrap for multiple samples.
+        if (isTRUE(multipleSamples)) {
+            p <- p + facet_wrap(facets = sym("ident"))
+        }
+        ## Return.
+        p
+    }
+
+
+
+#' @rdname plotCellCountsPerCluster
+#' @export
+setMethod(
+    f = "plotCellCountsPerCluster",
+    signature = signature(object = "SingleCellExperiment"),
+    definition = `plotCellCountsPerCluster,SCE`
+)
