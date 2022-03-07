9a01c2d24ea061e07c281187cf402cb4a499da9d
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-07 08:34:32 -0500

    Improve code coverage

--- tests/testthat/test-plotCountsPerBiotype.R
+++ tests/testthat/test-plotCountsPerBiotype.R
@@ -1,36 +1,15 @@
 context("plotCountsPerBiotype")
 
 test_that("RSE, SCE", {
+    transes <- eval(formals(`plotCountsPerBiotype,SE`)[["trans"]])
     for (object in list(rse, sce)) {
         x <- plotCountsPerBiotype(object)
         expect_s3_class(x, "ggplot")
-    }
-})
-
-transes <- eval(formals(`plotCountsPerBiotype,SE`)[["trans"]])
-test_that("trans", {
-    for (trans in transes) {
-        x <- plotCountsPerBiotype(rse, trans = trans)
-        expect_s3_class(x, "ggplot")
-    }
-})
-
-
-
-context("plotCountsPerBroadClass")
-
-test_that("RSE, SCE", {
-    for (object in list(rse, sce)) {
-        x <- plotCountsPerBroadClass(object)
-        expect_s3_class(x, "ggplot")
-    }
-})
-
-transes <-
-    eval(formals(`plotCountsPerBroadClass,SE`)[["trans"]])
-test_that("trans", {
-    for (trans in transes) {
-        x <- plotCountsPerBroadClass(rse, trans = trans)
-        expect_s3_class(x, "ggplot")
+        for (trans in transes) {
+            x <- plotCountsPerBiotype(object, trans = trans)
+            expect_s3_class(x, "ggplot")
+            x <- plotCountsPerBroadClass(object, trans = trans)
+            expect_s3_class(x, "ggplot")
+        }
     }
 })
