f81b4f7b5d21989f01533774af0a745fb86c97d4
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-11 14:28:03 -0400

    Improve coverage

--- tests/testthat/test-ggplot2-transforms.R
+++ tests/testthat/test-ggplot2-transforms.R
@@ -1,8 +1,6 @@
-test_that("acid_coord_flip", {
+test_that("acid_discrete_coord_flip", {
     p <- ggplot(data = mpg, aes(x = class)) +
-        geom_bar()
-    ## Note that this doesn't currently support `+` chain.
-    expect_type(acid_coord_flip, "closure")
-    p <- acid_coord_flip(p)
+        geom_bar() +
+        acid_discrete_coord_flip()
     expect_s3_class(p, "ggplot")
 })
